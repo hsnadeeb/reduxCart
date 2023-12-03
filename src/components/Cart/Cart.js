372fd74da96977fed502c20631fb02b79cbc5a6c
hsnadeeb <hsnadeeb007@gmail.com>
2023-12-03 15:50:32 +0530

    added fetch for cart as well

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,11 @@
+import { useSelector } from 'react-redux';
+
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
 
 const Cart = (props) => {
-  const cartItems = useSelector(state => state.cart.items);
+  const cartItems = useSelector((state) => state.cart.items);
 
   return (
     <Card className={classes.cart}>
@@ -12,8 +13,14 @@ const Cart = (props) => {
       <ul>
         {cartItems.map((item) => (
           <CartItem
-            key={item.itemId}
-            item={{ id: item.id, title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price }}
+            key={item.id}
+            item={{
+              id: item.id,
+              title: item.name,
+              quantity: item.quantity,
+              total: item.totalPrice,
+              price: item.price,
+            }}
           />
         ))}
       </ul>
